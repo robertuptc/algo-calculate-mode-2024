720909ff020625427df4bf50cace6b339eda10f2
robertuptc <robertuptc@gmail.com>
2024-06-07 13:05:36 -0400

    js problem finished

--- js/calculateMode.js
+++ js/calculateMode.js
@@ -1,3 +1,46 @@
-const calculateMode = () => {
+exports.calculateMode = function(arr) {
+    let obj = creating_obj(arr)
+    return findingMostFrequet(obj)
+}
 
+function creating_obj(arr) {
+    let obj = {}
+
+    for (i = 0; i < arr.length; i++) {
+        if ([arr[i]] in obj) {
+            obj[arr[i]] += 1
+        } else{
+            obj[arr[i]] = 1
+        }
+    }
+    return obj
+}
+
+function findingMostFrequet(obj) {
+    let counter = 0
+    let answer = []
+    for (const [key, value] of Object.entries(obj)) {
+        if (value > counter) {
+            answer = []
+            // if key can be converted into a number
+            if (Number(key)) {
+                counter = value
+                answer.push(Number(key))
+            } else if(key == '0'){
+                counter = value
+                answer.push(0)
+            }else {
+                counter = value
+                answer.push(key)
+            }
+        } else if (value == counter) {
+            if (Number(key)) {
+                answer.push(Number(key))
+            } else {
+                answer.push(key)
+            }
+        }
+    }
+    console.log(answer)
+    return answer
 }
--- js/calculateModeSpec.js
+++ js/calculateModeSpec.js
@@ -1,1 +1,9 @@
-// Write your own unit tests here!
+var cal = require("./calculateMode");
+
+cal.calculateMode([1,2,3,3]) == [3]
+cal.calculateMode([4.5, 0, 0]) == [0]
+cal.calculateMode([1.5, -1]) == [1.5] 
+cal.calculateMode([1.5, -1, 1, 1.5]) == [1.5] 
+cal.calculateMode([1,1,2,2]) == [1,2]
+cal.calculateMode([1,2,3]) == [1,2,3]
+cal.calculateMode(["who", "what", "where", "who"]) == ["who"]
